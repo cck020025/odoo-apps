5570e8d4bf231b96866ba1d9cd7d91430e5e07d2
cck020025 <cck020025@gmail.com>
2023-04-13 10:40:29 +0800

    sn to eq init

---
+++ cck_sn_to_eq/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Cck020025 (<https://github.com/cck020025>)
+{
+    "name": "Create Maintenance Equipment by SN",
+    'author': 'Cck020025',
+    'website': 'https://github.com/cck020025',
+    'version': '1.0.0',
+    'category': 'Inventory',
+    'license': 'LGPL-3',
+    'depends': ['stock', 'maintenance'],
+    'data': [
+        'views/stock_lot.xml',
+        'views/maintenance_equipment.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
